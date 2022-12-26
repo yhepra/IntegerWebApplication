f75572ca0600f777fff132edec957db38e2ab43c
Yoga Hendrapratama <yhepra@gmail.com>
2022-12-26 21:55:53 +0700

    Add Static Model

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,21 @@
+﻿using IntegerWebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IntegerWebApplication.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            var student = new Student()
+            {
+                Id = 1,
+                Name = "Dede",
+                Address = "Banjarsari City",
+                PhoneNumber = "1234567890"
+            };
+
+            return View(student);
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace IntegerWebApplication.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
